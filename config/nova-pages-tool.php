d5115a3cacb0aa00960fe2c8f70f93ae01327071
Ramiro Hammen <ramiro@grrr.nl>
2021-03-08 14:44:33 +0100

    Add publishable configuration for languages

---
+++ config/nova-pages-tool.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'languages' => ['en', 'nl', 'de'],
+    'defaultLanguage' => 'en',
+];
--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -24,6 +24,12 @@ class ToolServiceProvider extends ServiceProvider
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang/', 'pages');
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations/');
 
+        $this->publishes([
+            __DIR__ . '/../config/nova-pages-tool.php' => config_path(
+                'nova-pages-tool.php'
+            ),
+        ]);
+
         // Handle a page's URL composition.
         Event::listen(SavingPage::class, [UpdatePageUrl::class, 'handle']);
         Event::listen(SavedPage::class, [UpdateChildPageUrls::class, 'handle']);
@@ -31,5 +37,18 @@ class ToolServiceProvider extends ServiceProvider
             AttachTranslationBothWays::class,
             'handle',
         ]);
+    }
+
+    /**
+     * register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/nova-pages-tool.php',
+            'nova-pages-tool'
+        );
     }
 }

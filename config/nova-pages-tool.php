49cedd40400f81e7db3fd292b34aac165bd0a5d9
Harmen Janssen <harmen@grrr.nl>
2023-05-25 09:16:32 +0200

    Use FRONTEND_URL when linking page
    In a headless CMS, the pages shouldn't link to a relative URL, but use a
    full URL to the actual front-end host.

--- config/nova-pages-tool.php
+++ config/nova-pages-tool.php
@@ -14,4 +14,9 @@ return [
     'defaultTemplate' => \Grrr\Pages\Models\Page::TEMPLATE_DEFAULT,
 
     'seoImagesDisk' => env('SEO__DISK', 'public'),
+
+    // The URL to the front-end. Use this when you're using Nova as a headless
+    // CMS and the links from the CMS to the front-end should go to another
+    // host.
+    'frontendUrl' => env('FRONTEND_URL', ''),
 ];
